191989176a13548d78efa33e5e5b1ff02dc8565f
Blazmad <clement.madzar@epitech.eu>
2026-08-13 17:43:58 +0200

    test: prouve que shouldJiggle honore son paramètre de seuil

--- Tests/JiggleDecisionTests.swift
+++ Tests/JiggleDecisionTests.swift
@@ -17,4 +17,11 @@ final class JiggleDecisionTests: XCTestCase {
     func testDoesNotJiggleWhenFreshlyActive() {
         XCTAssertFalse(JiggleDecision.shouldJiggle(idleSeconds: 0, thresholdSeconds: 240))
     }
+
+    func testHonoursThresholdParameterRatherThanADefault() {
+        // Même inactivité, deux seuils différents, résultats opposés :
+        // une implémentation qui coderait 240 en dur échouerait ici.
+        XCTAssertTrue(JiggleDecision.shouldJiggle(idleSeconds: 150, thresholdSeconds: 120))
+        XCTAssertFalse(JiggleDecision.shouldJiggle(idleSeconds: 150, thresholdSeconds: 300))
+    }
 }
